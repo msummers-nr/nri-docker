522cf359dac2cfadd568386f7154ba5eba842796
Kav91 <kav.pather@gmail.com>
2018-10-31 09:07:43 +1100

    update linux install script

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+echo "New Relic Integration Installer"
 
-cp ./linux/nri-docker-config.yml /etc/newrelic-infra/integrations.d/
-cp ./linux/nri-docker-definition.yml /var/db/newrelic-infra/custom-integrations/
-cp ./linux/nri-docker /var/db/newrelic-infra/custom-integrations/
+if [ "$EUID" -ne 0 ]
+  then echo "Please run with sudo or as root"
+  exit
+fi
 
-service newrelic-infra stop
-service newrelic-infra start
+echo "Stopping NR Infrastructure Agent"
+if [ -f /etc/systemd/system/newrelic-infra.service ]; then
+    echo "SYSTEMD"
+    service newrelic-infra stop
+fi
+if [ -f /etc/init/newrelic-infra.conf ]; then
+    echo "INITCTL"
+    initctl stop newrelic-infra
+fi
+
+echo "Copying Files"
+cp ./nri-docker-config.yml /etc/newrelic-infra/integrations.d/
+cp ./nri-docker-def-nix.yml /var/db/newrelic-infra/custom-integrations/
+cp ./nri-docker /var/db/newrelic-infra/custom-integrations/
+
+echo "Starting NR Infrastructure Agent"
+if [ -f /etc/systemd/system/newrelic-infra.service ]; then
+    service newrelic-infra start
+fi
+if [ -f /etc/init/newrelic-infra.conf ]; then
+    initctl start newrelic-infra
+fi
